92b3156da28da6d0f6546580817e386943411377
skoop-n8n-continue <admin1+n8n-continue@skoop.digital>
2026-03-20 20:24:43 +0000

    update

--- fix_final.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let html = fs.readFileSync('index.html', 'utf8');
-
-html = html.replace(/                    if\(!confirm\("Are you sure you want to clear\/reset this app\?"\)\) return;\n\n\n/, '');
-
-fs.writeFileSync('index.html', html);
